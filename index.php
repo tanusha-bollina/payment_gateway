9bdb65cfa59114367d540cb8066d1e99270e010b
tanusha-bollina <69377076+tanusha-bollina@users.noreply.github.com>
2020-08-15 19:00:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <html>
 <head>
 <title>Donation</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
+	<meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
 </head>
 <?php include 'header.php'; ?>
 <link rel="stylesheet" href="style.css">
